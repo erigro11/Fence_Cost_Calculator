a9a23310c1eb2c778e3877eebabcf1342d105e36
erigro11 <101679354+erigro11@users.noreply.github.com>
2022-04-05 14:42:20 +1200

    Add files via upload

--- Fence_Cost_Calculator.py
+++ Fence_Cost_Calculator.py
@@ -1,4 +1,5 @@
 
+# checks input is a number more than zero
 def num_check(question):
     valid = False
     while not valid:
@@ -20,24 +21,35 @@ def num_check(question):
             print(error)
             print()
 
+# **** Main routine starts here ****
+
 print()
 print("***** Fence Cost Calculator *****")
 
 
+# program loops so we can do more than one calculation per session
 keep_going = ""
 while keep_going == "":
+
+
+    # Quetion
     print()
     print("What is the size of the")
     print("area you are fenceing?")
     print()
+
+    3
+    # get width,lenght and cost, checking they are numbers more than zero
     width = num_check("width: ")
     length = num_check("length: ")
     cost = num_check("Cost per meter: ")
 
+# Calculate perimeter and total 
     perimeter = 2 * (width + length)
 
     total = perimeter * cost
 
+# Output results to user
     print()
     print("the perimeter of the area you are fencing is {} meters".format(perimeter))
     print("the cost of the fence is ${} square units".format(total))
